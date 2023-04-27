75ef09fbbd59354561c487a38f2976aa4d0413bd
Amirali <amiralimesbah.pr@gmail.com>
2023-04-27 15:38:42 +0430

    writing some test for login and signup page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+class SignUpPageTest(TestCase):
+    def test_signup_page_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEquals(response.status_code, 200)
+    def test_signup_page_content(self):
+        response = self.client.get(reverse('signup'))
+        self.assertContains(response, 'Signup Page')
 
-# Create your tests here.
+class LoginPageView(TestCase):
+    def test_login_page_url_by_name(self):
+        response = self.client.get(reverse('login'))
+        self.assertEquals(response.status_code, 200)
+    def test_login_page_content(self):
+        response = self.client.get(reverse('login'))
+        self.assertContains(response, 'Login Page')
